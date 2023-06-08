24198fbd14a3b2ba427ea756ecb15cf436c70375
Miłosz Chojnacki <miloszchoj@gmail.com>
2023-06-08 17:22:34 +0200

    Initial commit

---
+++ src/main/java/com/mycompany/mavenproject2/BookBuyerAgent.java
@@ -0,0 +1,157 @@
+package com.mycompany.mavenproject2;
+
+import jade.core.Agent;
+import jade.core.AID;
+import jade.core.behaviours.*;
+import jade.lang.acl.*;
+
+// Przykładowa klasa zachowania:
+//class MyOwnBehaviour extends Behaviour
+//{
+//  protected MyOwnBehaviour()
+//  {
+//  }
+//
+//  public void action()
+//  {
+//  }
+//  public boolean done() {
+//    return false;
+//  }
+//}
+
+public class BookBuyerAgent extends Agent {
+
+    private String targetBookTitle;    // tytuł kupowanej książki przekazywany poprzez argument wejściowy
+    // lista znanych agentów sprzedających książki (w przypadku użycia żółtej księgi) - usługi katalogowej, sprzedawcy
+    // mogą być dołączani do listy dynamicznie!
+    private final AID[] sellerAgents = {
+            new AID("seller1", AID.ISLOCALNAME),
+            new AID("seller2", AID.ISLOCALNAME)};
+
+    // Inicjalizacja klasy agenta:
+    protected void setup()
+    {
+
+        //doWait(6000);   // Oczekiwanie na uruchomienie agentów sprzedających
+
+        System.out.println("Witam! Agent-kupiec "+getAID().getName()+" (wersja d <2020/21>) jest gotów!");
+
+        Object[] args = getArguments();  // lista argumentów wejściowych (tytuł książki)
+
+        if (args != null && args.length > 0)   // jeśli podano tytuł książki
+        {
+            targetBookTitle = (String) args[0];
+            System.out.println("Zamierzam kupić książkę zatytułowaną "+targetBookTitle);
+
+            addBehaviour(new RequestPerformer());  // dodanie głównej klasy zachowań — kod znajduje się poniżej
+
+        }
+        else
+        {
+            // Jeśli nie przekazano poprzez argument tytułu książki, agent kończy działanie:
+            System.out.println("Proszę podać tytuł lektury w argumentach wejściowych agenta kupującego!");
+            doDelete();
+        }
+    }
+    // Metoda realizująca zakończenie pracy agenta:
+    protected void takeDown()
+    {
+        System.out.println("Agent-kupiec "+getAID().getName()+" kończy.");
+    }
+
+    /**
+     Inner class RequestPerformer.
+     This is the behaviour used by Book-buyer agents to request seller
+     agents the target book.
+     */
+    private class RequestPerformer extends Behaviour
+    {
+
+        private AID bestSeller;     // agent sprzedający z najkorzystniejszą ofertą
+        private int bestPrice;      // najlepsza cena
+        private int repliesCnt = 0; // liczba odpowiedzi od agentów
+        private MessageTemplate mt; // szablon odpowiedzi
+        private int step = 0;       // krok
+
+        public void action()
+        {
+            ACLMessage reply;
+            switch (step) {
+                case 0 -> {      // wysłanie oferty kupna
+                    System.out.print(" Oferta kupna (CFP) jest wysyłana do: ");
+                    for (AID agent : sellerAgents) {
+                        System.out.print(agent + " ");
+                    }
+                    System.out.println();
+
+                    // Tworzenie wiadomości CFP do wszystkich sprzedawców:
+                    ACLMessage cfp = new ACLMessage(ACLMessage.CFP);
+                    for (AID sellerAgent : sellerAgents) {
+                        cfp.addReceiver(sellerAgent);                // dodanie adresata
+                    }
+                    cfp.setContent(targetBookTitle);                   // wpisanie zawartości — tytułu książki
+                    cfp.setConversationId("handel_ksiazkami");         // wpisanie specjalnego identyfikatora korespondencji
+                    cfp.setReplyWith("cfp" + System.currentTimeMillis()); // dodatkowa unikatowa wartość, żeby w razie odpowiedzi zidentyfikować adresatów
+                    myAgent.send(cfp);                           // wysłanie wiadomości
+
+
+                    // Utworzenie szablonu do odbioru ofert sprzedaży tylko od wskazanych sprzedawców:
+                    mt = MessageTemplate.and(MessageTemplate.MatchConversationId("handel_ksiazkami"),
+                            MessageTemplate.MatchInReplyTo(cfp.getReplyWith()));
+                    step = 1;     // przejście do kolejnego kroku
+                }
+                case 1 -> {      // odbiór ofert sprzedaży/odmowy od agentów-sprzedawców
+                    // odbiór odpowiedzi
+                    reply = myAgent.receive(mt);
+                    if (reply != null) {
+                        if (reply.getPerformative() == ACLMessage.PROPOSE)   // jeśli wiadomość jest typu PROPOSE
+                        {
+                            int price = Integer.parseInt(reply.getContent());  // cena książki
+                            if (bestSeller == null || price < bestPrice)       // jeśli jest to najlepsza oferta
+                            {
+                                bestPrice = price;
+                                bestSeller = reply.getSender();
+                            }
+                        }
+                        repliesCnt++;                                        // liczba ofert
+                        if (repliesCnt >= sellerAgents.length)               // jeśli liczba ofert co najmniej liczbie sprzedawców
+                        {
+                            step = 2;
+                        }
+                    } else {
+                        block();
+                    }
+                }
+                case 2 -> {      // wysłanie zamówienia do sprzedawcy, który złożył najlepszą ofertę
+                    ACLMessage order = new ACLMessage(ACLMessage.ACCEPT_PROPOSAL);
+                    order.addReceiver(bestSeller);
+                    order.setContent(targetBookTitle);
+                    order.setConversationId("handel_ksiazkami");
+                    order.setReplyWith("order" + System.currentTimeMillis());
+                    myAgent.send(order);
+                    mt = MessageTemplate.and(MessageTemplate.MatchConversationId("handel_ksiazkami"),
+                            MessageTemplate.MatchInReplyTo(order.getReplyWith()));
+                    step = 3;
+                }
+                case 3 -> {      // odbiór odpowiedzi na zamówienie
+                    reply = myAgent.receive(mt);
+                    if (reply != null) {
+                        if (reply.getPerformative() == ACLMessage.INFORM) {
+                            System.out.println("Tytuł " + targetBookTitle + " zamówiony!");
+                            System.out.println("Po cenie: " + bestPrice);
+                            myAgent.doDelete();
+                        }
+                        step = 4;
+                    } else {
+                        block();
+                    }
+                }
+            }
+        }
+
+        public boolean done() {
+            return ((step == 2 && bestSeller == null) || step == 4);
+        }
+    }
+}
---
+++ src/main/java/com/mycompany/mavenproject2/BookSellerAgent.java
@@ -0,0 +1,102 @@
+package com.mycompany.mavenproject2;
+
+import jade.core.Agent;
+import jade.core.behaviours.*;
+import jade.lang.acl.*;
+import java.util.*;
+import java.lang.*;
+
+public class BookSellerAgent extends Agent
+{
+    // Tworzenie katalogu lektur jako tablicy rozproszonej
+    // Katalog lektur na sprzedaż:
+    private final Hashtable<String, Integer> catalogue = new Hashtable<>();
+
+    // Inicjalizacja klasy agenta:
+    protected void setup()
+    {
+        Random randomGenerator = new Random();    // generator liczb losowych
+
+        catalogue.put("Zamek", 90+randomGenerator.nextInt(500));       // nazwa lektury jako klucz, cena jako wartość
+        catalogue.put("Opowiadania", 110+randomGenerator.nextInt(200));
+        catalogue.put("Ameryka", 300+randomGenerator.nextInt(70));
+        catalogue.put("Proces", 250+randomGenerator.nextInt(250));
+
+        doWait(2019);                     // czekaj 2 sekundy
+
+        System.out.println("Witam! Agent-sprzedawca (wersja b <2022/23>) "+getAID().getName()+" jest gotów do działania!");
+
+        // Dodanie zachowania obsługującego odpowiedzi na oferty klientów (kupujących książki):
+        addBehaviour(new OfferRequestsServer());
+
+        // Dodanie zachowania obsługującego zamówienie klienta:
+        addBehaviour(new PurchaseOrdersServer());
+    }
+
+    // Metoda realizująca zakończenie pracy agenta:
+    protected void takeDown()
+    {
+        System.out.println("Agent-sprzedawca (wersja b <2022/23>) "+getAID().getName()+" zakończył się.");
+    }
+
+
+    /**
+     Inner class OfferRequestsServer.
+     This is the behaviour used by Book-seller agents to serve incoming requests
+     for offer from buyer agents.
+     If the requested book is in the local catalogue the seller agent replies
+     with a PROPOSE message specifying the price. Otherwise a REFUSE message is sent back.
+     */
+    class OfferRequestsServer extends CyclicBehaviour
+    {
+        public void action()
+        {
+            // Tworzenie szablonu wiadomości (wstępne określenie tego, co powinna zawierać wiadomość)
+            MessageTemplate mt = MessageTemplate.MatchPerformative(ACLMessage.CFP);
+            // Próba odbioru wiadomości zgodnej z szablonem:
+            ACLMessage msg = myAgent.receive(mt);
+            if (msg != null) {  // jeśli nadeszła wiadomość zgodna z ustalonym wcześniej szablonem
+                String title = msg.getContent();  // odczytanie tytułu zamawianej książki
+
+                System.out.println("Agent-sprzedawca  "+getAID().getName()+" otrzymał komunikat: "+
+                        title);
+                ACLMessage reply = msg.createReply();               // tworzenie wiadomości — odpowiedzi
+                Integer price = (Integer) catalogue.get(title);     // ustalenie ceny dla podanego tytułu
+                if (price != null) {                                // jeśli taki tytuł jest dostępny
+                    reply.setPerformative(ACLMessage.PROPOSE);            // ustalenie typu wiadomości (propozycja)
+                    reply.setContent(String.valueOf(price.intValue()));   // umieszczenie ceny w polu zawartości (content)
+                    System.out.println("Agent-sprzedawca " + getAID().getName() + " odpowiada: " + price);
+                }
+                else {                                              // jeśli tytuł niedostępny
+                    // The requested book is NOT available for sale.
+                    reply.setPerformative(ACLMessage.REFUSE);         // ustalenie typu wiadomości (odmowa)
+                    reply.setContent("tytuł niestety niedostępny");                  // treść wiadomości
+                }
+                myAgent.send(reply);                                // wysłanie odpowiedzi
+            }
+            else                         // jeśli wiadomość nie nadeszła lub była niezgodna z szablonem
+            {
+                block();                   // blokada metody action() dopóki nie pojawi się nowa wiadomość
+            }
+        }
+    }
+
+
+    class PurchaseOrdersServer extends CyclicBehaviour
+    {
+        public void action()
+        {
+            ACLMessage msg = myAgent.receive();
+
+            if ((msg != null)&&(msg.getPerformative() == ACLMessage.ACCEPT_PROPOSAL))
+            {
+                // Message received. Process it
+                ACLMessage reply = msg.createReply();
+                String title = msg.getContent();
+                reply.setPerformative(ACLMessage.INFORM);
+                System.out.println("Agent sprzedający (wersja d <2021/22>) "+getAID().getName()+" sprzedał książkę: "+title);
+                myAgent.send(reply);
+            }
+        }
+    }
+}
